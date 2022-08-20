41639f05d8d5b11c3ad72c372b0e270b56bf5d53
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2022-08-20 14:40:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ import ReactGithubAPI from './ReactGithubAPI.jsx'
 // ReactDom.render(<h2>Hi</h2> ,document.getElementById('root'))
 
  ReactDom.render(<ReactGithubAPI/> ,document.getElementById('root'))
-
-//<App/>   This is component ,We always use first capital letter of component bcz it will differentiate html tags and react component 
